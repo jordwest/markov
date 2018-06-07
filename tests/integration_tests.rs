21c4d329a1b0975503b9523b614661a1d2f01204
Jordan West <jordwest@gmail.com>
2018-06-07 23:25:39 +1000

    fix tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,8 +4,8 @@ use markov::*;
 
 #[test]
 fn it_creates_a_markov_chain_and_ingests_a_string() {
-    let mut model = Model::new();
-    model.ingest("sometimes I like eating I also like dancing");
+    let mut model: Model<String> = Model::new();
+    model.ingest("sometimes I like eating I also like dancing".split_whitespace());
 
     assert_eq!(
         model.given("sometimes").chance_of("I"),
@@ -24,6 +24,6 @@ fn it_creates_a_markov_chain_and_ingests_a_string() {
 
     assert_eq!(
         model.given("sometimes").choose_random(),
-        Some(String::from("I")),
+        Some(&String::from("I")),
     );
 }
